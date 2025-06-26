901a9e4854bef7b6e0f4b581913250ce247bdd7e
Elia Salerno <hello@eliasalerno.ch>
2025-06-26 13:53:10 +0200

    Initial Commit

---
+++ fokusuhr-testing-platform Watch App/fokusuhr_testing_platformApp.swift
@@ -0,0 +1,17 @@
+//
+//  fokusuhr_testing_platformApp.swift
+//  fokusuhr-testing-platform Watch App
+//
+//  Created by Elia Salerno on 26.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct fokusuhr_testing_platform_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fokusuhr-testing-platform/fokusuhr_testing_platformApp.swift
@@ -0,0 +1,17 @@
+//
+//  fokusuhr_testing_platformApp.swift
+//  fokusuhr-testing-platform
+//
+//  Created by Elia Salerno on 26.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct fokusuhr_testing_platformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
