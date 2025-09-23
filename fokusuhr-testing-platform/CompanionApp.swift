1d7a8fde3c8988dce3e17f523a3354edbd1efb86
Elia Salerno <hello@eliasalerno.ch>
2025-09-23 21:08:15 +0200

    increase to version 1.0.4 pre-release

--- fokusuhr-testing-platform/CompanionApp.swift
+++ fokusuhr-testing-platform/CompanionApp.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 @main
 struct CompanionApp: App {
+    @StateObject private var watchConnector = WatchConnector()
     
     var body: some Scene {
         WindowGroup {
-            CompanionView()
+           CompanionView()
         }
     }
 }
+
+
