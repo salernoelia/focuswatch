1100f6d5816452fb1d1065a21e04679fa9c5e59d
Elia Salerno <hello@eliasalerno.ch>
2025-09-23 21:05:36 +0200

    fix: Switch to "Book" icon in Companion App for backwards compatibility

---
+++ fokusuhr-testing-platform/CompanionView.swift
@@ -0,0 +1,18 @@
+//
+//  CompanionView.swift
+//  fokusuhr-testing-platform
+//
+//  Created by Elia Salerno on 23.09.2025.
+//
+
+import SwiftUI
+
+struct CompanionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompanionView()
+}
