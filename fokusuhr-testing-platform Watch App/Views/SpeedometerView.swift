128ded36feb3df5abe2bfafb720b9bb992c405e4
Elia Salerno <hello@eliasalerno.ch>
2025-09-28 17:38:42 +0200

    add: foundation for Speedometer

---
+++ fokusuhr-testing-platform Watch App/Views/SpeedometerView.swift
@@ -0,0 +1,18 @@
+//
+//  SpeedometerView.swift
+//  fokusuhr-testing-platform Watch App
+//
+//  Created by Elia Salerno on 28.09.2025.
+//
+
+import SwiftUI
+
+struct SpeedometerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeedometerView()
+}
