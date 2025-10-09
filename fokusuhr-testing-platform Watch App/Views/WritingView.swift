08ec981ae5583032e778af54f3f77d02f87f69dc
Elia Salerno <hello@eliasalerno.ch>
2025-10-09 19:47:20 +0200

    RELEASE 1.1.1 — feat: implementation of prior FokusUhr Prototype

---
+++ fokusuhr-testing-platform Watch App/Views/WritingView.swift
@@ -0,0 +1,18 @@
+//
+//  WritingView.swift
+//  fokusuhr-testing-platform Watch App
+//
+//  Created by Elia Salerno on 09.10.2025.
+//
+
+import SwiftUI
+
+struct WritingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WritingView()
+}
