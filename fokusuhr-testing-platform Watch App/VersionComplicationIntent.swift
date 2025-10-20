0ba8140fb491bcc2a98f9091a4ffebeaa24ead65
Elia Salerno <hello@eliasalerno.ch>
2025-10-20 08:49:32 +0200

    refactor: version complication unification

--- fokusuhr-testing-platform Watch App/VersionComplicationIntent.swift
+++
@@ -1,7 +0,0 @@
-import AppIntents
-import WidgetKit
-
-struct VersionComplicationIntent: WidgetConfigurationIntent {
-  static var title: LocalizedStringResource { "App Version" }
-  static var description: IntentDescription { "Display FokusUhr app version" }
-}
