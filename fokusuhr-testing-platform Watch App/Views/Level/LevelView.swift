6259be65d78cf62f731d01b2b0d658bbff0c9ada
Elia Salerno <hello@eliasalerno.ch>
2025-11-04 21:03:39 +0100

    fix: Localization missmatches

---
+++ fokusuhr-testing-platform Watch App/Views/Level/LevelView.swift
@@ -0,0 +1,18 @@
+//
+//  LevelView.swift
+//  fokusuhr-testing-platform Watch App
+//
+//  Created by Elia Salerno on 04.11.2025.
+//
+
+import SwiftUI
+
+struct LevelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelView()
+}
