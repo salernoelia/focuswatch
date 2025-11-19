5ada4848876e5fdf4f842a309c67279a2b79259a
Elia Salerno <hello@eliasalerno.ch>
2025-11-19 09:35:12 +0100

    feat: separated Checklists view

---
+++ fokusuhr-testing-platform/Views/Wizard/WizardConfigView.swift
@@ -0,0 +1,18 @@
+//
+//  WizardConfigView.swift
+//  fokusuhr-testing-platform
+//
+//  Created by Elia Salerno on 19.11.2025.
+//
+
+import SwiftUI
+
+struct WizardConfigView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WizardConfigView()
+}
